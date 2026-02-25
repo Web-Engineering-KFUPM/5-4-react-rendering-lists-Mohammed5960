4e8a4005f2d289ffe854f3aa3be38b396d90e131
ALNASR <s202245960@ccm.kfupm.edu.sa>
2026-02-25 11:58:38 +0300

    5-4 corrected

--- 5-4-react-rendering-lists/src/components/DueBadge.jsx
+++ 5-4-react-rendering-lists/src/components/DueBadge.jsx
@@ -38,5 +38,5 @@ export default function DueBadge({ dueDate }) {
     label = `Due in ${d} days`;
   }
 
-  return <span className="badge">Label here</span>;
+  return <span className="badge">{label}</span>;
 }
